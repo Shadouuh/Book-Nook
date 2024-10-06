e60dd211c7028c027f646e0d8f632905fb7ccec8
Juanerros <138302286+Juanerros@users.noreply.github.com>
2024-10-05 22:01:28 -0300

    crud dinamico
    crud dinamita nena, ahora se puede hacer un crud a cada maldita tabla

--- server/app.js
+++ server/app.js
@@ -12,66 +12,102 @@ const conex = require(path.join(__dirname, 'config', 'conexBD'));
 app.use(express.json());
 app.use(cors());
 
-// Endpoints
-app.get('/', (req, res) => {
-    const query = 'SELECT * FROM libros';
+/*### Endpoints ###*/
+
+app.get('/api/:tabla', (req, res) => {
+    const { tabla } = req.params;
+    const query = 'SELECT * FROM ' + tabla;
 
     conex.query(query, (err, results) => {
         if (err) {
             console.error('Error al mostrar', err);
             res.status(500).send('Error en la consulta');
         } else {
-            res.send({ productos: results });
+            res.send({ resultados: results });
         }
     });
 });
 
-app.post('/libros', (req, res) => {
-    const { titulo, descripcion, precio, stock, es_fisico, fecha_publicacion, id_editorial, id_autor } 
-    = req.body;
-    const query = 
-    'INSERT INTO libros (titulo, descripcion, precio, stock, es_fisico, fecha_publicacion, id_editorial, id_autor) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+app.post('/api/:tabla', (req, res) => {
+    const { tabla } = req.params;
+    const datos = req.body;
 
-    conex.query(query, [titulo, descripcion, precio, stock, es_fisico, fecha_publicacion, id_editorial, id_autor ], (err, result) => {
-        if (err) {
-            console.error('Error al insertar el libro:', err);
-            res.status(500).send('Error al insertar el libro');
-        } else {
-            res.status(201).send(`Libro agregado con Id ${result.insertId}`);
-        }
+    const queryDesc = `DESC ${tabla}`;
+    conex.query(queryDesc, (err, result) => {
+        if (err) return res.status(500).send('Error al obtener las columnas de la tabla ' + tabla);
+
+        const columnas = result.map(col => col.Field);
+        const valores = columnas.map(col => datos[col]);
+
+        const query = `INSERT INTO ${tabla} (${columnas.join(', ')}) VALUES (${columnas.map(() => '?').join(', ')})`;
+
+        conex.query(query, valores, (err, result) => {
+            if (err) return res.status(500).send('Error al insertar los datos');
+
+            res.send(`Datos insertados en la tabla ${tabla}`);
+        });
     });
 });
 
-app.put('/libros/:id', (req, res) => {
-    const { id } = req.params;  
-    const { titulo, descripcion, precio, stock, es_fisico, fecha_publicacion, id_editorial, id_autor  } = req.body;
-    const query = 'UPDATE libros SET titulo = ?, descripcion = ?, precio = ?, stock = ?, es_fisico = ?, fecha_publicacion = ?, id_editorial = ?, id_autor = ?  WHERE id_libro = ?';
 
-    conex.query(query, [titulo, descripcion, precio, stock, es_fisico, fecha_publicacion, id_editorial, id_autor, id], (err, result) => {
-        if (err) {
-            console.error('Error al actualizar el libro:', err);
-            res.status(500).send('Error al actualizar el libro');
-        } else if (result.affectedRows === 0) {
-            res.status(404).send('Libro no encontrado');
-        } else {
-            res.send(`Libro con el Id ${id} actualizado`);
-        }
+app.put('/api/:tabla/:id', (req, res) => {
+    const { tabla, id } = req.params;
+    const datos = req.body;
+
+    const queryDesc = `DESC ${tabla}`;
+    conex.query(queryDesc, (err, result) => {
+        if (err) return res.status(500).send('Error al obtener las columnas de la tabla ' + tabla);
+        const columnas = result.map(col => col.Field);
+        const valores = columnas.map(col => datos[col]);
+
+        result.map(function (col) {
+            if (col.Key == "PRI") primary = col.Field;
+        });
+
+        columnas.shift();
+        valores.shift();
+
+        columnas[columnas.length - 1] = columnas[columnas.length - 1] + " = ?";
+
+        const query = `UPDATE ${tabla} SET ${columnas.join(" = ?,")} WHERE ${primary} = ?`;
+
+        valores.push(id);
+
+        conex.query(query, valores, (err, result) => {
+            if (err) {
+                console.error('Error al actualizar el elemento:', err);
+                res.status(500).send('Error al actualizar el elemento');
+            } else if (result.affectedRows === 0) {
+                res.status(404).send('elemento no encontrado');
+            } else {
+                res.send(`elemento con el ${primary} ${id} actualizado`);
+            }
+        });
     });
+
 });
 
-app.delete('/libros/:id', function (req, res) {
-    const { id } = req.params;
-    const query = 'DELETE FROM libros WHERE id_libro = ?';
+app.delete('/api/:tabla/:id', (req, res) => {
+    const { tabla, id } = req.params;
 
-    conex.query(query, [id], (err, results) => {
-        if (err) {
-            console.error('Error al eliminar el libro:', err);
-            res.status(500).send('Error al eliminar el libro');
-        } else if (results.affectedRows === 0) {
-            res.status(404).send('Libro no encontrado');
-        } else {
-            res.send(`Libro con el ID ${id} eliminado`);
-        }
+    const queryDesc = 'DESC ' + tabla;
+    conex.query(queryDesc, (err, result) => {
+        if (err) return res.status(500).send('Error al obtener las columnas de la tabla ' + tabla);
+        result.map(function (col) {
+            if (col.Key == "PRI") primary = col.Field;
+        });
+        const query = `DELETE FROM ${tabla} WHERE ${primary} = ?`;
+
+        conex.query(query, [id], (err, results) => {
+            if (err) {
+                console.error('Error al eliminar:', err);
+                res.status(500).send('Error al eliminar');
+            } else if (results.affectedRows === 0) {
+                res.status(404).send('elemento con id no encontrado');
+            } else {
+                res.send(`Elemento con el ${primary} ${id} eliminado`);
+            }
+        });
     });
 });
 
