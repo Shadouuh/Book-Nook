a7f0860a8f53738c89e00944021f33793af7dd9a
Juanerros <juancottier0@gmail.com>
2024-10-14 21:14:10 -0300

    verificacions y arreglos
    la verdad no me acuerdo bien lo que cambie, pero fue una refactirizacion del codigo

--- server/app.js
+++ server/app.js
@@ -21,16 +21,26 @@ app.use(cors());
 app.post('/login', (req, res) => {
     const { user } = req.body;
     const clave = anju.encrypt(user.clave);
-    const query = "SELECT id_login, tipo FROM login WHERE email=? OR telefono=? AND clave=?";
 
-    conex.query(query, [user.email, user.telefono, clave], (err, results) => {
-        if (err) return res.status(401).send({ message: 'Error en el login', error: err });
-        else if (results == "") {
-            res.send({ message: 'Credenciales incorrectas' });
+    const verifi = "SELECT id_login FROM login WHERE email=? OR telefono=?";
+
+    conex.query(verifi, [user.email, user.telefono], (err, result) => {
+        if (err) return res.status(500).send({ message: 'Error en la verificación', error: err });
+        else if (result == "") {
+            res.status(401).send({ message: 'No existe cuenta con el email ' + user.email });
+        } else {
+            const query = "SELECT id_login, tipo FROM login WHERE email=? OR telefono=? AND clave=?";
+
+            conex.query(query, [user.email, user.telefono, clave], (err, results) => {
+                if (err) return res.status(500).send({ message: 'Error en el login', error: err });
+                else if (results == "") {
+                    res.send({ message: 'Credenciales incorrectas' });
+                }
+                else {
+                    res.send({ resultado: results });
+                };
+            });
         }
-        else {
-            res.send({ resultado: results });
-        };
     });
 });
 
@@ -39,24 +49,35 @@ app.post('/register', (req, res) => {
     const { user } = req.body;
     const clave = anju.encrypt(user.clave);
 
-    const verifi = "SELECT id_login FROM login WHERE email=?";
+    const verifiMail = "SELECT id_login FROM login WHERE email=?";
 
-    conex.query(verifi, [user.email], (err, result) => {
-        if (err) return res.status(500).send({ message: 'Error en la verificación' });
+    conex.query(verifiMail, [user.email], (err, result) => {
+        if (err) return res.status(500).send({ message: 'Error en la verificación del email', error: err });
         else if (result != "") {
             res.status(401).send({ message: `El email: ${user.email} ya esta tiene cuenta` });
         } else {
-            const query = "INSERT INTO login(email, telefono, clave, tipo) VALUES(?, ?, ?, 'cliente');";
+            const queryTel = "SELECT telefono FROM login WHERE telefono=?";
 
-            conex.query(query, [user.email, user.telefono, clave], (err, results) => {
-                if (err) return res.status(500).send({ message: 'Error al insertar en login', error: err });
-                else {
-                    let hoy = new Date().toLocaleDateString();
-                    const query2 = `INSERT INTO usuarios(nombre, apellido, direccion, fecha_registro, fecha_nacimiento, alias, id_login) VALUES(?,?,?,'${hoy}' ,?,?, ${results.insertId})`;
-                    conex.query(query2, [user.nombre, user.apellido, user.direccion, user.fecha_nacimiento, user.alias], (err, result) => {
-                        if (err) return res.status(500).send({ message: 'Error al insertar en usuarios', error: err });
+            conex.query(queryTel, [user.telefono], (err, results) => {
+                if (err) return res.status(500).send({ message: 'Error en la verificacion del telefono', error: err });
+                else if (user.telefono && results != "") {
+                    if (results[0].telefono == user.telefono) {
+                        res.status(401).send({ message: `El telefono: ${user.telefono} ya esta registrado` });
+                    }
+                } else {
+                    const query = "INSERT INTO login(email, telefono, clave, tipo) VALUES(?, ?, ?, 'cliente');";
+
+                    conex.query(query, [user.email, user.telefono, clave], (err, results) => {
+                        if (err) return res.status(500).send({ message: 'Error al insertar en login', error: err });
                         else {
-                            res.status(201).send({ message: 'Se registro correctamente el usuario' })
+                            let hoy = new Date().toLocaleDateString();
+                            const query2 = `INSERT INTO usuarios(nombre, apellido, direccion, fecha_registro, fecha_nacimiento, alias, id_login) VALUES(?,?,?,'${hoy}' ,?,?, ${results.insertId})`;
+                            conex.query(query2, [user.nombre, user.apellido, user.direccion, user.fecha_nacimiento, user.alias], (err, result) => {
+                                if (err) return res.status(500).send({ message: 'Error al insertar en usuarios', error: err });
+                                else {
+                                    res.status(201).send({ message: 'Se registro correctamente el usuario' });
+                                }
+                            });
                         }
                     });
                 }
@@ -73,7 +94,7 @@ app.post('/api/libros/guardar', (req, res) => {
     conex.query(verifi, (err, result) => {
         if (err) return res.status(500).send({ message: 'Error en la consulta de verificación', error: err });
         else if (result != "") {
-            res.status(500).send({ message: 'El libro ya esta guardado' });
+            res.status(401).send({ message: 'El libro ya esta guardado' });
         } else {
             const query = `INSERT INTO usuario_libro(id_libro, id_usuario) VALUES('${save.id_libro}', '${save.id_usuario}')`;
 
@@ -168,6 +189,9 @@ app.get('/api/:tabla/:id', (req, res) => {
 
         conex.query(query, (err, results) => {
             if (err) return res.status(500).send({ message: 'Error en la consulta', error: err });
+            else if (results == "") {
+                res.status(404).send({ message: 'No se encontro el elemento' });
+            }
             else res.send({ resultados: results });
         });
     });
@@ -180,6 +204,9 @@ app.get('/:tabla/desc', (req, res) => {
 
     conex.query(queryDesc, (err, result) => {
         if (err) return res.status(500).send({ message: 'Error al obtener las columnas de la tabla ' + tabla, error: err });
+        else if (result == "") {
+            res.status(404).send({ message: 'No se encontro la tabla ' + tabla });
+        }
         else {
             res.status(200).send({ resultado: result });
         }
@@ -204,19 +231,22 @@ app.post('/api/:tabla', (req, res) => {
     const queryDesc = 'DESC ' + tabla;
     conex.query(queryDesc, (err, result) => {
         if (err) return res.status(500).send({ message: 'Error al obtener las columnas de la tabla ' + tabla, error: err });
+        else if (result == "") {
+            res.status(404).send({ message: 'No se encontro la tabla ' + tabla });
+        } else {
+            const columnas = result.map(col => col.Field);
+            const valores = columnas.map(col => dates[col]);
 
-        const columnas = result.map(col => col.Field);
-        const valores = columnas.map(col => dates[col]);
+            const campos = result.map(col => col.Field);
+            const tipos = result.map(col => col.Type);
 
-        const campos = result.map(col => col.Field);
-        const tipos = result.map(col => col.Type);
+            const query = `INSERT INTO ${tabla} (${columnas.join(', ')}) VALUES (${columnas.map(() => '?').join(', ')})`;
 
-        const query = `INSERT INTO ${tabla} (${columnas.join(', ')}) VALUES (${columnas.map(() => '?').join(', ')})`;
-
-        conex.query(query, valores, (err, result) => {
-            if (err) return res.status(500).send({ message: 'Error al insertar los datos', error: err });
-            res.status(201).send({ message: 'Se inserto correctamente en ' + tabla });
-        });
+            conex.query(query, valores, (err, result) => {
+                if (err) return res.status(500).send({ message: 'Error al insertar los datos', error: err });
+                res.status(201).send({ message: 'Se inserto correctamente en ' + tabla });
+            });
+        }
     });
 });
 
@@ -227,25 +257,28 @@ app.put('/api/:tabla/:id', (req, res) => {
     const queryDesc = 'DESC ' + tabla;
     conex.query(queryDesc, (err, result) => {
         if (err) return res.status(500).send({ message: 'Error al obtener las columnas de la tabla ' + tabla, error: err });
+        else if (result == "") {
+            res.status(404).send({ message: 'No se encontro la tabla ' + tabla });
+        } else {
+            const columnas = result.map(col => col.Field);
+            const valores = columnas.map(col => dates[col]);
 
-        const columnas = result.map(col => col.Field);
-        const valores = columnas.map(col => dates[col]);
+            result.map(function (col) {
+                if (col.Key == "PRI") primary = col.Field;
+            });
 
-        result.map(function (col) {
-            if (col.Key == "PRI") primary = col.Field;
-        });
+            columnas.shift();
+            valores.shift();
 
-        columnas.shift();
-        valores.shift();
+            const query = `UPDATE ${tabla} SET ${columnas.join(" = ?,")}  = ? WHERE ${primary} = ${id}`;
 
-        const query = `UPDATE ${tabla} SET ${columnas.join(" = ?,")}  = ? WHERE ${primary} = ${id}`;
-
-        conex.query(query, valores, (err, result) => {
-            if (err) return res.status(500).send({ message: 'Error al actualizar el elemento', error: err });
-            else if (result.affectedRows == 0) {
-                res.status(404).send({ message: 'elemento no encontrado' });
-            } else res.status(201).send({ message: `elemento con el ${primary} ${id} actualizado` });
-        });
+            conex.query(query, valores, (err, result) => {
+                if (err) return res.status(500).send({ message: 'Error al actualizar el elemento', error: err });
+                else if (result.affectedRows == 0) {
+                    res.status(404).send({ message: 'elemento no encontrado' });
+                } else res.status(201).send({ message: `elemento con el ${primary} ${id} actualizado` });
+            });
+        }
     });
 });
 
@@ -255,17 +288,21 @@ app.delete('/api/:tabla/:id', (req, res) => {
     const queryDesc = 'DESC ' + tabla;
     conex.query(queryDesc, (err, result) => {
         if (err) return res.status(500).send({ message: 'Error al obtener las columnas de la tabla ' + tabla, error: err });
-        result.map(function (col) {
-            if (col.Key == "PRI") primary = col.Field;
-        });
-        const query = `DELETE FROM ${tabla} WHERE ${primary} = ?`;
+        else if (result == "") {
+            res.status(404).send({ message: 'No se encontro la tabla ' + tabla });
+        } else {
+            result.map(function (col) {
+                if (col.Key == "PRI") primary = col.Field;
+            });
+            const query = `DELETE FROM ${tabla} WHERE ${primary} = ?`;
 
-        conex.query(query, [id], (err, results) => {
-            if (err) return res.status(500).send({ message: 'Error al eliminar', error: err });
-            else if (results.affectedRows == 0) {
-                res.status(404).send({ message: 'elemento con id no encontrado' });
-            } else res.send({ message: `Elemento con el ${primary} ${id} eliminado` });
-        });
+            conex.query(query, [id], (err, results) => {
+                if (err) return res.status(500).send({ message: 'Error al eliminar', error: err });
+                else if (results.affectedRows == 0) {
+                    res.status(404).send({ message: 'elemento con id no encontrado' });
+                } else res.send({ message: `Elemento con el ${primary} ${id} eliminado` });
+            });
+        }
     });
 });
 
