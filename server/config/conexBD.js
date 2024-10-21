cb94611b4990218cfc285b771e4b6febd553b214
Juanerros <juancottier0@gmail.com>
2024-10-21 16:45:00 -0300

    resubido
    que tocaste santi

--- server/config/conexBD.js
+++ server/config/conexBD.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const mysql = require('mysql2/promise');
-
-async function createConnection() {
-    try {
-        const conex = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'book-nook-bd'
-        });
-
-        console.log('Conexion exitosa!!!');
-        return conex;
-
-    } catch (err) {
-        console.error('Error en la conexion', err);
-        throw err;
-    }
-};
-
-=======
 const mysql = require('mysql2/promise');
 
 async function createConnection() {
@@ -40,5 +18,4 @@ async function createConnection() {
     }
 };
 
->>>>>>> 5a60ae304e9bad0a897c2d236f77bc1e75e2996b
 module.exports = createConnection;
